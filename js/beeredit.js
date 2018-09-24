dd715944e93e03abf3632bae30fbda6d4c798705
Aaron Walton <aaron.walton@sap.com>
2018-09-24 16:20:14 -0400

    more mods

--- js/beeredit.js
+++ js/beeredit.js
@@ -2,48 +2,39 @@ var serviceURL = "http://beertest.aaronwalton.org/services/";
 
 $(document).on('pageshow', '#editPage', function(event) {
 	var id = getUrlVars()["id"];
-//	$.getJSON(serviceURL + 'getlocations.php?id='+id, displayLocations);
-//	$.getJSON(serviceURL + 'getlists.php?id='+id, displayLists);
-	$.getJSON(serviceURL + 'getservingtypess.php?id='+id, displayServingTypes);
-
+	//	$.getJSON(serviceURL + 'getlocations.php?id='+id, displayLocations);
+	//	$.getJSON(serviceURL + 'getlists.php?id='+id, displayLists);
+	$.getJSON(serviceURL + 'getservingtypes.php', displayServingTypes);
 	$.getJSON(serviceURL + 'getbeer.php?id='+id, displayBeer);
-//	$.getJSON(serviceURL + 'getservings.php?id='+id, displayServings);
+	//	$.getJSON(serviceURL + 'getservings.php?id='+id, displayServings);
 });
 
 function displayBeer(data) {
-    var beer = data.item;
-    console.log(beer);
+	var beer = data.item;
+	console.log(beer);
 
-    $('#beerPic').attr('src', 'images/hooters.png');
-		$('#beerid').textinput(beer.beer_id);
-    $('#beerName').text(beer.Name);
-		$('#beerURL').text(beer.BeerAdvocate);
-//    if (beer.BeerAdvocate) {
-//       $('#actionList').append('<li><a href="' + beer.BeerAdvocate + '"><h3>Beer Advocate</h3>' +
-//				'</a></li>');
-//    }
-//    $('#beerCharacter').append(icons(beer.Characteristics + ' ' + beer.CellarServing));
-		$('beerCharacter').textinput(beer.Characteristics);
-//    if (beer.cellared > 0) {
-       $('#beerCellared').text(beer.cellared);
-       $('#beerCellarDate').text(beer.CellarDate);
-			 // need to add ServingTypoes in dropdown and then
-       $('#beerServ').text('This beer is cellared in ' + beer.CellarServing);
-//    }
-		if (beer.ExtendedCellar == "1")
-    	 $('#flipswitch-HighGrav').flipswitch("Yes");
-	  else {
-			 $('#flipswitch-HighGrav').flipswitch("No");
-		}
-		$('#flipswitch-HighGrav').flipswitch('refresh');
+	$('#beerPic').attr('src', 'images/hooters.png');
+	$('#beerid').text(beer.beer_id);
+	$('#beerid').textinput('refresh');
+	$('#beerName').text(beer.Name);
+	$('#beerName').textinput('refresh');
+	$('#beerURL').text(beer.BeerAdvocate);
+	$('#beerURL').textinput('refresh');
+	$('#beerCharacter').text(beer.Characteristics);
+	$('#beerCharacter').textinput('refresh');
+	$('#beerCellared').text(beer.cellared);
+	$('#beerCellared').textinput('refresh');
+	$('#beerCellarDate').text(beer.CellarDate);
 
-    if (beer.photo_id > 1) {
-        $('#actionList').append('<li>Photo_Id</h3> is ' + beer.photo_id + '</li>');
-    }
-    $('#actionList').listview('refresh');
+	// if (beer.ExtendedCellar == "1")
+	// 	$('#flipswitch-HighGrav').prop("checked") = true;
+	// else
+	// 	$('#flipswitch-HighGrav').prop("checked") = false;
 
-    $('#notes').textarea(beer.Notes);
-    $('#notes').textinput('refresh');
+	$('#flipswitch-HighGrav').flipswitch('refresh');
+
+	$('#notes').text(beer.Notes);
+	$('#notes').textinput('refresh');
 }
 
 // function displayServings(data) {
@@ -75,19 +66,24 @@ function displayBeer(data) {
 // }
 
 function displayServingTypes(data) {
-  var servingtypes = data.items;
-  console.log(servingtypes);
+	var servingtypes = data.items;
+	console.log('displayServingTypes')
+	console.log(servingtypes);
 
+	$.each(servingtypes, function(index, servingtype) {
+		$('#serving').append('<option value="' + servingtype.Name + '">' + servingtype.Name + '</option>');
+	});
+	$('#serving').refresh();
 }
 
 function getUrlVars() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
+	var vars = [], hash;
+	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	for(var i = 0; i < hashes.length; i++)
+	{
+		hash = hashes[i].split('=');
+		vars.push(hash[0]);
+		vars[hash[0]] = hash[1];
+	}
+	return vars;
 }
